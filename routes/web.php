84faaa038ef94c71b2e434492ec8f80b4287a6d5
yuikoto <soraifu.com@gmail.com>
2022-10-17 00:14:53 +0900

    あいさつアプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/comments/random', [App\Http\Controllers\GreetingControllers::class, 'comments2']);
+//変数を先に書いてしまうと、CSSのように上から読み込まれてしまうためこのファイルの1番上に書く or GreetingControllers.phpにrandomを記載する。
+
+Route::get('/comments/{greet}', [App\Http\Controllers\GreetingControllers::class, 'comments']);
+
+Route::get('/comments/freeword/{greet}', [App\Http\Controllers\GreetingControllers::class, 'comments1']);
